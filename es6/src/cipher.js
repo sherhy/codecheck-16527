e9637d245a9f0af9abe669e3df369c1f52afa8c4
hyunjin <hyunjin.jeong@toki.waseda.jp>
2017-09-14 14:33:11 +0900

    where can i test javascript? this is becomming a pain to check via git push

--- es6/src/cipher.js
+++ es6/src/cipher.js
@@ -15,7 +15,10 @@ export class CaesarCipher extends Cipher {
 		let newstr = str.toLowerCase();
 		let enscription = [];
 		newstr.forEach(function(t){
-			let secret = t.charCodeAt(0) + this.shift;
+			let secret = t.charCodeAt(0);
+			if(secret < 123 && secret > 96){
+				secret = secret+ this.shift;
+			}
 			if (secret > 122){
 				let difference = secret -122;
 				secret = 96 + difference;
